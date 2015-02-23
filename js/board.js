36da9a839c22f76463e8e316fa3da43fc2a8c7f8
Stefanie Lynch <stefanielynch@Stefanie-Lynchs-MacBook-Pro-2.local>
2015-02-22 19:15:09 -0800

    make board, grid

--- js/board.js
+++ js/board.js
@@ -3,4 +3,21 @@
     window.TFE = {};
   }
 
+  function Board () {
+    this.grid = Board.makeGrid();
+  };
+
+  Board.makeGrid = function () {
+    var grid = [];
+
+    for (var i = 0; i < 4; i++) {
+      grid.push([]);
+      for (var j = 0; j < 4; j++) {
+        grid[i].push(null);
+      }
+    }
+
+    return grid;
+  };
+
 })
