d67e31635205460696c84d07af6a311edbaabfe9
Adam Berkovec <ajberk@gmail.com>
2015-02-22 20:28:26 -0800

    made a grid and put some squares in it

--- js/2048-view.js
+++ js/2048-view.js
@@ -3,8 +3,29 @@
     window.TFE = {};
   }
 
+  var View = TFE.View = function(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+    this.bindEvents();
+  };
 
+  View.prototype.bindEvents = function() {
+    // click handlers and stuff
+  };
 
+  View.prototype.setupBoard = function() {
+    var $container = $("<div>");
+    $container.addClass("container");
 
-
-});
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 4; j++) {
+        var $square = $("<div>");
+        $square.addClass("square")
+        $square.data("pos", [i,j]);
+        $container.append($square)
+      }
+    }
+    this.$el.append($container)
+  };
+})();
--- js/board.js
+++ js/board.js
@@ -1,23 +1,14 @@
-(function() {
-  if (typeof TFE === "undefined") {
-    window.TFE = {};
-  }
+function Board() {
+  this.grid = Board.makeGrid();
+};
 
-  function Board () {
-    this.grid = Board.makeGrid();
-  };
-
-  Board.makeGrid = function () {
-    var grid = [];
-
-    for (var i = 0; i < 4; i++) {
-      grid.push([]);
-      for (var j = 0; j < 4; j++) {
-        grid[i].push(null);
-      }
+Board.makeGrid = function () {
+  var grid = [];
+  for (var i = 0; i < 4; i++) {
+    grid.push([]);
+    for (var j = 0; j < 4; j++) {
+      grid[i].push(null);
     }
-
-    return grid;
-  };
-
-})
+  }
+  return grid;
+};
