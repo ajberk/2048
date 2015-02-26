f950431a50ec32e2d89beba1e859d3856a1403e5
Stefanie Lynch <stefanielynch@Stefanie-Lynchs-MacBook-Pro-2.local>
2015-02-26 15:11:31 -0800

    doesn't lose right

--- js/game.js
+++ js/game.js
@@ -42,12 +42,54 @@
   Game.prototype.checkWinner = function() {
     for (var i = 0; i < this.board.grid.length; i++) {
       for (var j = 0; j < this.board.grid.length; j++) {
-        if (this.board.grid[i][j] !== null && this.board.grid[i][j].val === 64) {
+        if (this.board.grid[i][j] !== null && this.board.grid[i][j].val === 2048) {
           alert("Winner!")
         }
       }
     }
   }
+
+  Game.prototype.checkLoser = function() {
+    for (var i = 0; i < this.board.grid.length; i++) {
+      for (var j = 0; j < this.board.grid.length; j++) {
+         if (this.board.grid[i][j] === null) {
+            return false;
+         }
+       }
+     }
+     if (this.movesPossible()) {
+       return false;
+     }
+    alert("You lost, but great effort!");
+    return true;
+   }
+
+  Game.prototype.sameValueNeighbor = function(i,j) {
+    if (
+      (this.isOnBoard(i+1, j) && this.board.grid[i][j].val === this.board.grid[i+1][j].val) ||
+      (this.isOnBoard(i-1, j) && this.board.grid[i][j].val === this.board.grid[i-1][j].val) ||
+      (this.isOnBoard(i, j+1) && this.board.grid[i][j].val === this.board.grid[i][j+1].val) ||
+      (this.isOnBoard(i, j-1) && this.board.grid[i][j].val === this.board.grid[i][j-1].val)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  Game.prototype.isOnBoard = function(i, j) {
+    return (i >= 0 && i<=3 && j >= 0 && j<=3)
+  }
+
+  Game.prototype.movesPossible = function() {
+    for (var i = 0; i < this.board.grid.length; i++) {
+      for (var j = 0; j < this.board.grid.length; j++) {
+        if (this.isOnBoard(i, j) && this.sameValueNeighbor(i, j)) {
+          return true;
+        }
+          return false;
+        }
+      }
+    }
 
   Game.prototype.move = function(key_dir) {
     var copy = this.gridCopy();
@@ -67,6 +109,7 @@
     }
     this.checkWinner();
     this.addPieceIfMoved(copy);
+    this.checkLoser();
     this.allCombinable();
   };
 
